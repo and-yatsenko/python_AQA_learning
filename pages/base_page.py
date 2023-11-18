c6f8b737f2fcf7d2d292d2e904edae31dfcce7a4
Andrey Yatsenko <Yatsen3655@mail.ru>
2023-11-18 18:30:03 +0700

    Add Page Object

---
+++ pages/base_page.py
@@ -0,0 +1,8 @@
+class BasePage():
+    def __init__(self, browser, url):
+        self.browser = browser
+        self.url = url
+        self.browser.implicitly_wait(10)
+
+    def open(self):
+        self.browser.get(self.url)
