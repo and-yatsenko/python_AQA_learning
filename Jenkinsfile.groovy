176e9fa0002288b97672c0c991478a7f1ed79833
Andrey Yatsenko <Yatsen3655@mail.ru>
2023-11-28 18:30:28 +0700

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -7,13 +7,19 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                checkout scmGit(branches: [[name: '${branch_name}']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/and-yatsenko/python_AQA_learning.git']])
+                checkout scmGit(branches: [[name: '${TEST_BRANCH_NAME}']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/and-yatsenko/python_AQA_learning.git']])
             }
         }
         stage('Build') {
             steps {
                  // Создание виртуальной среды и установка пакетов
                 sh 'python3 -m venv venv'
+                sh '. venv/bin/activate && pip install -r requirements.txt'
+            }
+        }
+        stage('Test') {
+            steps {
+                // Запуск автотестов
                 script {
                     if (${params.TEST_SUITE} == 'Smoke') {
                         sh '. venv/bin/activate && pytest -s -v -m smoke --alluredir allure-results'
@@ -21,12 +27,6 @@ pipeline {
                         sh '. venv/bin/activate && pytest -s -v -m "smoke and regression" --alluredir allure-results'
                     }
                 }
-            }
-        }
-        stage('Test') {
-            steps {
-                // Запуск автотестов (например, pytest)
-                sh '. venv/bin/activate && pytest -s -v -m "${TEST_BRANCH_NAME}" --alluredir allure-results'
             }
         }
         stage('Reports') {
